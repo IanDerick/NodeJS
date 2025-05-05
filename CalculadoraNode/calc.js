cf3f83ba191e1989d1347e1617b1d5462e34b494
IanDerick <ian.derick@outlook.com>
2025-05-05 19:52:26 -0300

    master

---
+++ CalculadoraNode/calc.js
@@ -0,0 +1,11 @@
+let SomaFunc = require("./somar");
+console.log(`O valor da soma é: ${SomaFunc(1,2)}`);
+
+let DivideFunc = require("./divisao");
+console.log(`O valor da divisão é: ${DivideFunc(4,2)}`);
+
+let SubtracaoFunc = require("./subtracao");
+console.log(`O valor da subtração é: ${SubtracaoFunc(2,1)}`);
+
+let multiplaFunc = require("./multiplicacao");
+console.log(`O valor da multiplicação é: ${multiplaFunc(5,2)}`);
