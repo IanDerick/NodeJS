809c6e4012cb6905ac461cb0ab0bcbf5f3617441
IanDerick <ian.derick@outlook.com>
2025-05-06 19:39:55 -0300

    Aprendendo a utilizar expressJS junto com Thunder Client

---
+++ ListaDeGames/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servidor rodando!");
+});
+
+let games = [
+    {title: "Sea Of Thieves", studio: "Rare",  price: 30},
+    {title: "WOW", studio: "Blizzard", price: 120},
+    {title: "Valorant", studio: "Riot", price: 0},
+    {title: "COD", studio: "Activision", price: 200},
+    {title: "Minecraft", studio: "Mojang", price: 80},
+    {title: "Halo", studio: "Microsoft", price: 90},
+    {title: "GTA", studio: "Rockstar", price: 150},
+    {title: "Red Dead Redamption", studio: "Rockstar", price: 200},
+    {title: "Skate 3", studio: "EA", price: 80},
+    {title: "Forza Horizon 5", studio: "Microsoft", price: 120},
+];
+
+app.get('/', (req, res) => {
+    res.json(games);
+})
