f94c8c922d870e6ff1f7c4b0f410cc8841622b76
IanDerick <ian.derick@outlook.com>
2025-05-07 16:01:48 -0300

    Fazendo metodos GET, POST, PUT e DELETE para carros

--- http/index.js
+++ http/index.js
@@ -1,22 +1,54 @@
 import express from 'express';
 const  app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => 
-console.log('Servidor iniciado na porta 3000')
-);
+var carros = [
+    {marca: "Ford", modelo: "Fiesta", preco: 45000},
+    {marca: "VW", modelo: "Gol", preco: 43000},
+    {marca: "Honda", modelo: "Civic", preco: 95000},
+    {marca: "Toyota", modelo: "Corolla", preco: 100000},
+    {marca: "Chevrolet", modelo: "Onix", preco: 70000},
+    {marca: "Argo", modelo: "Fiat", preco: 68000},
+];
+
 
 app.get('/', (req, res) => 
-res.send('<h1 style="color: blue"> CRIANDO UM SERVIDOR COM EXPRESS.JS<h1>')
+    res.send("<h3>Rotas no Express</h3><p>Rota '/'")
 );
 
-app.get('/sobre', (req, res) =>
-res.send('<h1 style="color: blue">PÁGINA SOBRE<h1>')
-);
+app.post('/cars/novo', (req, res) => {
+    let marca = req.body.marca;
+    let modelo = req.body.modelo;
+    let preco = req.body.preco;
 
-app.get('/contato', (req, res) => 
-res.send('<h1 style="color: blue">PÁGINA CONTATO<h1>')
-);
+    console.log(marca);
+    console.log(modelo);
+    console.log(preco);
 
-app.get('/empresa', (req, res) => 
-res.send('<h1 style="color: blue">PÁGINA EMPRESA<h1>')
+    let novoCarro = {marca, modelo, preco};
+
+    carros.push(novoCarro);
+
+    res.send("OK");
+});
+
+app.get('/cars', (req, res) => {
+    return res.json(carros)
+});
+
+app.put('/cars/update/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    carros[id] = req.body;
+    return res.json(carros[id]);
+});
+
+app.delete('/cars/delete/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const removido = carros.splice(id, 1);
+
+    return res.json({ removido }); 
+});
+
+app.listen(3000, () => 
+    console.log('Servidor iniciado na porta 3000')
 );
